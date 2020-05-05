d67423cca733efe26f7542b6de17d25d1a79e309
Vladimir Romanovich <ibnteo@gmail.com>
2020-05-05 17:30:08 +0300

    v0.2

--- firmware/slidest.ino
+++ firmware/slidest.ino
@@ -1,7 +1,7 @@
 /*
  * Touch slide keyboard Slidest (сенсорная слайдовая клавиатура Слайдость)
- * Version: 0.1 alpha
- * Date: 2020-05-03
+ * Version: 0.2 alpha
+ * Date: 2020-05-05
  * Description: https://github.com/ibnteo/slidest (soon)
  * Author: Vladimir Romanovich <ibnteo@gmail.com>
  * License: MIT
@@ -20,8 +20,18 @@ uint16_t list_touched[2] = {0};
 uint8_t index_touched[2] = {0};
 
 struct Chord {
-  uint16_t c; // chord 2*5 bit
+  uint16_t c; // chord 5*3 bit
   byte s;     // symbol
+};
+
+struct Chord2 {
+  uint16_t c; // chord 5*3 bit
+  byte s[4];  // symbols in 4 layers
+};
+
+struct ChordM {
+  uint16_t c; // chord 5*3 bit
+  String s;   // macros
 };
 
 #define S1 0
@@ -31,45 +41,85 @@ struct Chord {
 #define S5 12
 
 // Consonants
-#define CHORD4 sizeof(chord4) / 3
-Chord chord4[] = {
-  {(2<<S1), 's'},
-  {(3<<S1), 'n'},
-  {(6<<S1), 't'},
-  {(1<<S1)|(2<<S2), 'h'},
-  {(1<<S1)|(3<<S2), 'r'},
-  {(5<<S1)|(3<<S2), 'l'},
-  {(5<<S1)|(6<<S2), 'c'},
-  {(3<<S1)|(5<<S2)|(6<<S3), 'p'},
-  {(3<<S1)|(5<<S2)|(3<<S3), 'b'},
-  {(3<<S1)|(1<<S2)|(2<<S3), 'm'},
-  {(3<<S1)|(1<<S2)|(3<<S3), 'k'},
-  {(2<<S1)|(1<<S2)|(3<<S3), 'z'},
-  {(2<<S1)|(1<<S2)|(2<<S3), 'j'},
-  {(6<<S1)|(5<<S2)|(4<<S3), 'd'},
-  {(6<<S1)|(5<<S2)|(6<<S3), 'g'},
+#define CHORD4 sizeof(chord4) / 6
+Chord2 chord4[] = {
+  {(2<<S1), {'s', 'c', '2', 0}},
+  {(3<<S1), {'n', 'y', 0, 0}},
+  {(6<<S1), {'t', 'n', '1', 0}},
+  {(1<<S1)|(2<<S2), {'h', 'd', 0, 0}},
+  {(1<<S1)|(3<<S2), {'r', 'h', 0, 0}},
+  {(5<<S1)|(3<<S2), {'l', 'k', 0, 0}},
+  {(5<<S1)|(6<<S2), {'c', 'r', 0, 0}},
+  {(3<<S1)|(5<<S2)|(6<<S3), {'p', 'g', '{', 0}},
+  {(3<<S1)|(5<<S2)|(3<<S3), {'b', ',', 0, 0}},
+  {(3<<S1)|(1<<S2)|(2<<S3), {'m', 'v', '[', 0}},
+  {(3<<S1)|(1<<S2)|(3<<S3), {'k', 'x', 0, 0}},
+  {(2<<S1)|(1<<S2)|(3<<S3), {'z', 'p', '+', 0}},
+  {(2<<S1)|(1<<S2)|(2<<S3), {'j', 'q', '8', 0}},
+  {(6<<S1)|(5<<S2)|(3<<S3), {'d', 'l', '-', 0}},
+  {(6<<S1)|(5<<S2)|(6<<S3), {'g', 'u', '9', 0}},
+  {(1<<S1)|(3<<S2)|(5<<S3)|(6<<S4), {0, ';', 0, 0}},
+  {(1<<S1)|(3<<S2)|(5<<S3)|(3<<S4), {0, 'i', 0, 0}},
+  {(1<<S1)|(3<<S2)|(1<<S3)|(2<<S4), {0, 'a', 0, 0}},
+  {(1<<S1)|(2<<S2)|(1<<S3)|(3<<S4), {0, '\'', 0, 0}},
+  {(5<<S1)|(3<<S2)|(1<<S3)|(2<<S4), {0, '[', 0, 0}},
+  {(5<<S1)|(3<<S2)|(1<<S3)|(3<<S4), {0, 'o', 0, 0}},
+  {(5<<S1)|(3<<S2)|(5<<S3)|(6<<S4), {0, 'w', 0, 0}},
+  {(5<<S1)|(6<<S2)|(5<<S3)|(3<<S4), {0, ']', 0, 0}},
+  {(3<<S1)|(1<<S2)|(2<<S3)|(1<<S4)|(3<<S5), {'-', '-', 0, 0}},
+  {(3<<S1)|(5<<S2)|(6<<S3)|(5<<S4)|(3<<S5), {'"', '@', 0, 0}},
+  {(3<<S1)|(1<<S2)|(3<<S3)|(5<<S4)|(6<<S5), {'_', '_', 0, 0}},
+  {(3<<S1)|(5<<S2)|(3<<S3)|(1<<S4)|(2<<S5), {'\'', '@', 0, 0}},
+  {(2<<S1)|(1<<S2)|(3<<S3)|(5<<S4)|(6<<S5), {',', '?', 0, 0}},
+  {(6<<S1)|(5<<S2)|(3<<S3)|(1<<S4)|(2<<S5), {'.', '/', 0, 0}},
+  {(2<<S1)|(1<<S2)|(3<<S3)|(1<<S4)|(2<<S5), {'(', '(', 0, 0}},
+  {(6<<S1)|(5<<S2)|(3<<S3)|(5<<S4)|(6<<S5), {')', ')', 0, 0}},
+  {(2<<S1)|(1<<S2)|(3<<S3)|(5<<S4)|(3<<S5), {';', '$', 0, 0}},
+  {(6<<S1)|(5<<S2)|(3<<S3)|(1<<S4)|(3<<S5), {':', '^', 0, 0}},
 };
 
 // Vowels
-#define CHORD0 sizeof(chord0) / 3
-Chord chord0[] = {
-  {(4<<S1)|(2<<S2), 'o'},
-  {(4<<S1)|(6<<S2), 'e'},
-  {(4<<S1)|(3<<S2), ' '},
-  {(4<<S1)|(2<<S2)|(1<<S3), 'i'},
-  {(4<<S1)|(6<<S2)|(5<<S3), 'a'},
-  {(4<<S1)|(3<<S2)|(5<<S3), 'u'},
-  {(4<<S1)|(3<<S2)|(1<<S3), 'y'},
+#define CHORD0 sizeof(chord0) / 6
+Chord2 chord0[] = {
+  {(4<<S1)|(2<<S2), {'o', 'j', '0', 0}},
+  {(4<<S1)|(6<<S2), {'e', 't', '3', 0}},
+  {(4<<S1)|(3<<S2), {' ', ' ', ' ', 0}},
+  {(4<<S1)|(2<<S2)|(1<<S3), {'i', 'b', '4', 0}},
+  {(4<<S1)|(6<<S2)|(5<<S3), {'a', 'f', '5', 0}},
+  {(4<<S1)|(3<<S2)|(5<<S3), {'u', 'e', '6', 0}},
+  {(4<<S1)|(3<<S2)|(1<<S3), {'y', 'z', '7', 0}},
+  {(4<<S1)|(3<<S2)|(1<<S3)|(2<<S4), {0, 's', 0, 0}},
+  {(4<<S1)|(2<<S2)|(1<<S3)|(3<<S4), {0, 'm', '}', 0}},
+  {(4<<S1)|(3<<S2)|(5<<S3)|(6<<S4), {0, '.', 0, 0}},
+  {(4<<S1)|(6<<S2)|(5<<S3)|(3<<S4), {0, '`', ']', 0}},
 };
 
 // Vowels + space
-#define CHORD0SP sizeof(chord0sp) / 3
-Chord chord0sp[] = {
-  {(4<<S1)|(2<<S2)|(1<<S3)|(2<<S4), 'i'},
-  {(4<<S1)|(6<<S2)|(5<<S3)|(6<<S4), 'a'},
-  {(4<<S1)|(3<<S2)|(5<<S3)|(3<<S4), 'u'},
-  {(4<<S1)|(3<<S2)|(1<<S3)|(3<<S4), 'y'},
+#define CHORD0SP sizeof(chord0sp) / 6
+Chord2 chord0sp[] = {
+  {(4<<S1)|(2<<S2)|(1<<S3)|(2<<S4), {'i', 'b', '4', 0}},
+  {(4<<S1)|(6<<S2)|(5<<S3)|(6<<S4), {'a', 'f', '5', 0}},
+  {(4<<S1)|(3<<S2)|(5<<S3)|(3<<S4), {'u', 'e', '6', 0}},
+  {(4<<S1)|(3<<S2)|(1<<S3)|(3<<S4), {'y', 'z', '7', 0}},
+  {(4<<S1)|(3<<S2)|(1<<S3)|(2<<S4)|(1<<S5), {0, 's', 0, 0}},
+  {(4<<S1)|(2<<S2)|(1<<S3)|(3<<S4)|(1<<S5), {0, 'm', '}', 0}},
+  {(4<<S1)|(3<<S2)|(5<<S3)|(6<<S4)|(5<<S5), {0, '.', 0, 0}},
+  {(4<<S1)|(6<<S2)|(5<<S3)|(3<<S4)|(5<<S5), {0, '`', ']', 0}},
 };
+
+// Macros
+#define CHORDM sizeof(chordm) / 8
+ChordM chordm[] = {
+  {(4<<S1)|(3<<S2)|(1<<S3)|(2<<S4), "ia"},
+  {(4<<S1)|(2<<S2)|(1<<S3)|(3<<S4), "ao"},
+  {(4<<S1)|(3<<S2)|(5<<S3)|(6<<S4), "ue"},
+  {(4<<S1)|(6<<S2)|(5<<S3)|(3<<S4), "you"},
+};
+
+#define KEY_LAYOUT 255
+#define KEY_LAYOUT_NUM 254
+byte layout = 0;
+bool layout_num = false;
 
 // Controls
 #define CHORD0C sizeof(chord0c) / 3
@@ -80,18 +130,39 @@ Chord chord0c[] = {
   {(5<<S1)|(1<<S2), KEY_UP_ARROW},
   {(5<<S1)|(6<<S2), KEY_RIGHT_ARROW},
   {(6<<S1)|(5<<S2), KEY_LEFT_ARROW},
+  {(1<<S1)|(2<<S2), KEY_LAYOUT_NUM},
+  {(1<<S1)|(2<<S2)|(1<<S3), KEY_LAYOUT},
+  {(3<<S1)|(5<<S2)|(3<<S3), KEY_PAGE_DOWN},
+  {(5<<S1)|(3<<S2)|(5<<S3), KEY_PAGE_UP},
+};
+
+// Ctrl + Controls
+#define CHORD0CC sizeof(chord0cc) / 3
+Chord chord0cc[] = {
+  {(2<<S1)|(1<<S2)|(2<<S3), KEY_BACKSPACE},
+  {(1<<S1)|(3<<S2)|(1<<S3), KEY_RETURN},
+  {(5<<S1)|(6<<S2)|(5<<S3), KEY_RIGHT_ARROW},
+  {(6<<S1)|(5<<S2)|(6<<S3), KEY_LEFT_ARROW},
 };
 
 void press4(uint8_t k) { // Autopress on sector 4
   Serial.print(list_touched[k], BIN);
   Serial.println(" press4");
+  byte l = layout_num ? 2 : layout;
   if ((list_touched[k] & 0b111) == 4) {
-    for (uint8_t i = 0; i < CHORD0; i ++) {
+    for (byte i = 0; i < CHORD0; i ++) {
       if (list_touched[k] == chord0[i].c) {
-        if (chord0[i].s == ' ') {
+        if (chord0[i].s[l] == 0) {
+          for (byte j = 0; j < CHORDM; j ++) {
+            if (list_touched[k] == chordm[j].c) {
+              Keyboard.print(chordm[j].s);
+              break;
+            }
+          }
+        } else if (chord0[i].s[l] == ' ') {
           Keyboard.write(KEY_TAB);
         } else {
-          Keyboard.write(chord0[i].s);
+          Keyboard.write(chord0[i].s[l]);
           Keyboard.write(' ');
         }
         break;
@@ -100,7 +171,7 @@ void press4(uint8_t k) { // Autopress on sector 4
   } else {
     for (uint8_t i = 0; i < CHORD4; i ++) {
       if (list_touched[k] == chord4[i].c) {
-        Keyboard.write(chord4[i].s);
+        Keyboard.write(chord4[i].s[l]);
         break;
       }
     }
@@ -110,31 +181,90 @@ void press4(uint8_t k) { // Autopress on sector 4
 void press0(uint8_t k) { // Press on release
   Serial.print(list_touched[k], BIN);
   Serial.println(" press0");
+  byte l = layout_num ? 2 : layout;
   if ((list_touched[k] & 0b111) == 4) {
     for (uint8_t i = 0; i < CHORD0; i ++) {
       if (list_touched[k] == chord0[i].c) {
-        Keyboard.write(chord0[i].s);
+        if (chord0[i].s[l] == 0) {
+          for (byte j = 0; j < CHORDM; j ++) {
+            if (list_touched[k] == chordm[j].c) {
+              Keyboard.print(chordm[j].s);
+              break;
+            }
+          }
+        } else {
+          Keyboard.write(chord0[i].s[l]);
+          if (chord0[i].s[l] == ' ') {
+            layout_num = false;
+          }
+        }
         break;
       }
     }
     for (uint8_t i = 0; i < CHORD0SP; i ++) {
       if (list_touched[k] == chord0sp[i].c) {
-        Keyboard.write(chord0sp[i].s);
+        Keyboard.write(chord0sp[i].s[l]);
         Keyboard.write(' ');
+        layout_num = false;
         break;
       }
     }
   } else {
     for (uint8_t i = 0; i < CHORD0C; i ++) {
       if (list_touched[k] == chord0c[i].c) {
-        Keyboard.write(chord0c[i].s);
+        Serial.println(chord0c[i].s, HEX);
+        if (chord0c[i].s == KEY_LAYOUT_NUM) {
+          Serial.println(layout_num, BIN);
+          layout_num = ! layout_num;
+          Serial.println(layout_num, BIN);
+        } else if (chord0c[i].s == KEY_LAYOUT) {
+          Serial.println("Layout");
+          layout2(layout ? 0 : 1);
+        } else {
+          Keyboard.write(chord0c[i].s);
+        }
+        break;
+      }
+    }
+    for (uint8_t i = 0; i < CHORD0CC; i ++) {
+      if (list_touched[k] == chord0cc[i].c) {
+        Serial.println("Ctrl");
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(chord0cc[i].s);
+        Keyboard.releaseAll();
         break;
       }
     }
   }
 }
 
+#define LED_LAYOUT LED_BUILTIN_RX
+#define LINUX   0
+#define WINDOWS 1
+#define MACOS   2
+#define OS LINUX
+void layout2(byte layer) {
+  if (layout != layer) {
+    Keyboard.releaseAll();
+    #if OS == MACOS
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(' ');
+    #elif OS == WINDOWS
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_SHIFT);
+    #else
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(KEY_LEFT_SHIFT);
+    #endif
+    Keyboard.releaseAll();
+  }
+  layout = layer;
+  digitalWrite(LED_LAYOUT, layout == 0 ? HIGH : LOW); // LOW = light
+}
+
 void setup() {
+  pinMode(LED_BUILTIN_TX, INPUT); // disable LED TX
+  pinMode(LED_LAYOUT, OUTPUT);
   Keyboard.begin();
   sensors.begin(0x5A);
 }
@@ -155,7 +285,7 @@ void loop() {
     j = j & 0b111;
     
     if ((curr_touched & (1 << i)) && ! (last_touched & (1 << i)) ) { // Touch
-      if (list_touched && (j == 4)) {
+      if (list_touched[k] && (j == 4)) {
         press4(k);
         index_touched[k] = 3;
         list_touched[k] = 4;
